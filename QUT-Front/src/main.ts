e1c4bbbb131fe28bdb3927e47416e1f3cd028fe9
luochen <luochen@bsoft.com>
2025-10-15 10:55:23 +0800

    网站图标更新

--- QUT-Front/src/main.ts
+++ QUT-Front/src/main.ts
@@ -3,12 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+//这是引入vue的app组件，和router路由组件
 import App from './App.vue'
 import router from './router'
+
+//这是引入antd的组件库
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
