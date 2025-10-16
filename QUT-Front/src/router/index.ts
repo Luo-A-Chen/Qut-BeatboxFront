46d934422b2b4f8e6ba365be2db819af88ec040f
luochen <luochen@bsoft.com>
2025-10-16 22:28:07 +0800

    前端页面的更新

--- QUT-Front/src/router/index.ts
+++ QUT-Front/src/router/index.ts
@@ -1,22 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
+import UserLoginPage from '@/pages/user/UserLoginPage.vue'
+import AboutPage from '@/pages/AboutPage.vue'
+import UserRegisterPage from '@/pages/user/UserRegisterPage.vue'
+import UserManagePage from '@/pages/user/UserManagePage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: '主页',
       component: HomePage,
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../pages/AboutPage.vue'),
+      name: '关于',
+      component: AboutPage.vue,
     },
+    {
+      path: '/user/login',
+      name: '用户登录',
+      component: UserLoginPage,
+    },
+    {
+      path: '/user/register',
+      name: '用户注册',
+      component:UserRegisterPage.vue,
+    },
+    {
+      path: '/admin/userManage',
+      name: '用户管理',
+      component:UserManagePage.vue,
+    },
+
   ],
 })
 
